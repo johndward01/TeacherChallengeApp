99b5f9ccd71c31f295d27765148df63eebafe516
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-08-06 16:32:14 -0500

    Added extra comments in the Method summaries

--- TeacherChallengeLibrary/ApiManager.cs
+++ TeacherChallengeLibrary/ApiManager.cs
@@ -10,11 +10,15 @@ namespace TeacherChallengeLibrary
     {
         private static Random rng = new Random();
 
+        /// <summary>
+        /// Mock api call, the method itself doesn't really matter, the challenge is how to alert the user something happened from inside this method
+        /// Hint - Console.WriteLine() does not belong in here. We want that logic only inside of the Console project. Think of it this way, we have 2 UI projects
+        /// (Console and WinForms) and we want to be able to alert the user. If we just said Console.WriteLine
+        /// we wouldn't be able to alert the user in the WinForms proj and vice versa.
+        /// </summary>
         public void DoApiCall()
         {
-            //Doing an api call.... 
-            
-
+            //Doing an api call....
 
             //OOOPS, something went wrong, or maybe it went right, maybe I just need to let the user know something, I need to tell the user in the console...
             //But HOW? That logic is in my ConsoleLogging class in the Console project
